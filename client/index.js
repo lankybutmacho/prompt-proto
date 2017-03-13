75e0e56617ecff53edc3fe0cba9f67883bf20efe
Matt Healy <matt@pickaxe.nyc>
2017-03-13 13:30:00 -0400

    Built with :heart: by BitMate - [http://bitmate.io]

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
+import {Main} from './app/main';
+import './index.scss';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}/>
+  </Router>,
+  document.getElementById('root')
+);
